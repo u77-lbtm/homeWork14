ca05b41a8d249105661a01963f775edcf8610fdb
u77-lbtm <kraftrr77@gmail.com>
2026-08-21 14:15:16 +0500

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
 public class Main {
-    String Author;
+    Author author;
     String name;
     int yearPublish;
     public static void main(String[] args) {
-        Book book1 = new Book("Brain Backs ", "Black Hat ", 2020);
+        Author brainBacks = new Author("Brain", "Backs");
+        Author brainRed = new Author("Brain", "Red");
+        Author dolevFarhi = new Author("Dolev", "Farhi");
+        Author alexRobachevsky = new Author("Alex", "Robachevsky");
+
+        Book book1 = new Book(brainBacks , "Black Hat", 2020);
         System.out.println("book1.getAuthorName() = " + book1.getAuthorName());
         System.out.println("book1.getName() = " + book1.getName());
         System.out.println("book1.getYearPublish() = " + book1.getYearPublish());
         System.out.println("");
 
-        Book book2 = new Book("Brain Red ", "Red Hat ", 2023);
+        Book book2 = new Book(brainRed , "Red Hat", 2023);
         System.out.println("book2.getAuthorName() = " + book2.getAuthorName());
         System.out.println("book2.getName() = " + book2.getName());
         System.out.println("book2.getYearPublish() = " + book2.getYearPublish());
         System.out.println("");
 
-        Book book3 = new Book("Dolev Farhi", "Blach Hat Bash ", 2023);
+        Book book3 = new Book(dolevFarhi, "Black Hat Bash ", 2023);
         System.out.println("book3.getAuthorName() = " + book3.getAuthorName());
         System.out.println("book3.getName() = " + book3.getName());
         System.out.println("book3.getYearPublish() = " + book3.getYearPublish());
@@ -23,11 +28,11 @@ public class Main {
 
 
         Author author1 = new Author("Alex ", "Robachevsky ");
-        System.out.println("author1.getFirstName() = " + author1.getFirstName());
-        System.out.println("author1.getLastName() = " + author1.getLastName());
+        System.out.println("author1.getFirstName() = " + alexRobachevsky.getFirstName());
+        System.out.println("author1.getLastName() = " + alexRobachevsky.getLastName());
+        System.out.println("author2 = " + dolevFarhi);
+        System.out.println("");
 
-        Author author2 = new Author("Dolev ","Farhi ");
-        System.out.println("author2 = " + author2);
         System.out.println("");
 
         book3.setYearPublish(2024);
